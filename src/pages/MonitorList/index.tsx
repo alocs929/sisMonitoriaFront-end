ab209fd1f8e3fbaf747f893d1943d9253cda4dbd
Sarah Ferreira <sarahfssilva@hotmail.com>
2021-03-27 13:44:08 -0300

    base page list monitores

--- src/pages/MonitorList/index.tsx
+++ src/pages/MonitorList/index.tsx
@@ -1,9 +1,34 @@
-import React from 'react'
+import React, { useState, FormEvent } from  'react';
 
-function MonitorList() {
-    return (
-        <h1>MonitorList</h1>
-    );
+import PageHeader from '../../components/PageHeader';
+
+import './styles.css';
+import TeacherItem, {Teacher} from '../../components/TeacherItem';
+import Input from '../../components/Input';
+import Select from '../../components/Select';
+import api from '../../services/api';
+
+function MonitorList(){
+  const [teachers, setTeachers] = useState([]);
+
+  return(
+    <>
+      <div id="page-teacher-list" className="container">
+        
+        <PageHeader title="Estes são os monitores disponíveis">
+          
+        </PageHeader>
+
+        <main>
+          {teachers.map((teacher: Teacher) =>{
+            return(
+              <TeacherItem key={teacher.id} teacher={teacher}/>
+            );
+          })}
+        </main>
+      </div>
+    </>
+  );
 }
 
 export default MonitorList;
